3081cad6e51f69d39d5020d09a34d7f9ef3662bc
Andrii Kurshatsov <andreswebit@ukr.net>
2023-07-07 20:14:53 +0300

    Update gallery 1-2

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,3 +1,4 @@
+//Створи галерею з можливістю кліку по її елементах і перегляду повнорозмірного зображення у модальному вікні.
 import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
@@ -23,19 +24,24 @@ const galleryListMarkup = galleryItems
 //Додаємо розмітку фотогалереї на сайт
 galleryList.insertAdjacentHTML("afterbegin", galleryListMarkup);
 
+//Додаємо случач по кліку
 galleryList.addEventListener("click", onGalleryListClick);
 
 function onGalleryListClick(event) {
   event.preventDefault();
 
+  //Якщо елемент не картинка return
   if (!event.target.classList.contains("gallery__image")) {
     return;
   }
 
+  //Якщо картинка, то виводимо Lightbox
   const instance = basicLightbox.create(`
     <img src="${event.target.dataset.source}" alt="${event.target.alt}" class="gallery__size">`);
 
   instance.show();
+
+  //Додаємо слухач по натискані ESC
   document.addEventListener("keydown", onEscPress);
 
   function onEscPress(event) {
