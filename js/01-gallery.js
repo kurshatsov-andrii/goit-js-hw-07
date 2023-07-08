91c531633b011cc6b922731728c3405e5f08ca57
Andrii Kurshatsov <andreswebit@ukr.net>
2023-07-08 15:43:34 +0300

    Update gallery 1

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -27,27 +27,60 @@ galleryList.insertAdjacentHTML("afterbegin", galleryListMarkup);
 //Додаємо случач по кліку
 galleryList.addEventListener("click", onGalleryListClick);
 
+// function onGalleryListClick(event) {
+//   event.preventDefault();
+
+//   //Якщо елемент не картинка return
+//   if (!event.target.classList.contains("gallery__image")) {
+//     return;
+//   }
+
+//   //Якщо картинка, то виводимо Lightbox
+//   const instance = basicLightbox.create(`
+//     <img src="${event.target.dataset.source}" alt="${event.target.alt}" class="gallery__size">`);
+
+//   instance.show();
+
+//   //Додаємо слухач по натискані ESC
+//   document.addEventListener("keydown", onEscPress);
+
+//   function onEscPress(event) {
+//     if (event.code === "Escape") {
+//       instance.close();
+//       document.removeEventListener("keydown", onEscPress);
+//     }
+//   }
+// }
+
+//Другий варіант
+
+const instance = basicLightbox.create(
+  `
+     <img src="" alt="" class="gallery__size">`,
+  {
+    onShow: (instance) => {
+      document.addEventListener("keydown", onEscPress);
+      document.querySelector("body").classList.add("no-scroll");
+    },
+
+    onClose: (instance) => {
+      document.removeEventListener("keydown", onEscPress);
+      document.querySelector("body").classList.remove("no-scroll");
+    },
+  }
+);
+
 function onGalleryListClick(event) {
   event.preventDefault();
 
-  //Якщо елемент не картинка return
-  if (!event.target.classList.contains("gallery__image")) {
-    return;
-  }
+  if (!event.target.classList.contains("gallery__image")) return;
 
-  //Якщо картинка, то виводимо Lightbox
-  const instance = basicLightbox.create(`
-    <img src="${event.target.dataset.source}" alt="${event.target.alt}" class="gallery__size">`);
-
+  instance.element().querySelector("img").src = event.target.dataset.source;
+  instance.element().querySelector("img").alt = event.target.alt;
   instance.show();
+}
 
-  //Додаємо слухач по натискані ESC
-  document.addEventListener("keydown", onEscPress);
-
-  function onEscPress(event) {
-    if (event.code === "Escape") {
-      instance.close();
-      document.removeEventListener("keydown", onEscPress);
-    }
-  }
+function onEscPress(event) {
+  if (event.code !== "Escape") return;
+  instance.close();
 }
